607fe6e7bac167c16a3279ccec98b2e3ab5dbbfa
Tim McEwan <timmcewandev@gmail.com>
2024-10-28 22:01:17 -0600

    initial

---
+++ Fetch/FetchApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchApp.swift
+//  Fetch
+//
+//  Created by Tim McEwan on 10/28/24.
+//
+
+import SwiftUI
+
+@main
+struct FetchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FetchTests/FetchTests.swift
@@ -0,0 +1,17 @@
+//
+//  FetchTests.swift
+//  FetchTests
+//
+//  Created by Tim McEwan on 10/28/24.
+//
+
+import Testing
+@testable import Fetch
+
+struct FetchTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
