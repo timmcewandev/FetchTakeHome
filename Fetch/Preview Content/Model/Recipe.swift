e68f7dad4df0eeb41549382ee90165c8698b9fff
Tim McEwan <timmcewandev@gmail.com>
2024-10-29 00:03:34 -0600

    Adding in working components and networking layer. Going to have to kingfisher dialed in order to get images.

---
+++ Fetch/Preview Content/Model/Recipe.swift
@@ -0,0 +1,36 @@
+//
+//  Recipe.swift
+//  Fetch
+//
+//  Created by Tim McEwan on 10/28/24.
+//
+
+import Foundation
+
+struct Recipes: Codable, Identifiable {
+    let uuid: String
+    let name: String
+    let cuisine: String
+    let photoUrlLarge: String?
+    let photoUrlSmall: String?
+    let sourceUrl: String?
+    let youtubeUrl: String?
+    
+    var id: String { uuid }
+    
+    enum CodingKeys: String, CodingKey {
+        case uuid
+        case name
+        case cuisine
+        case photoUrlLarge = "photo_url_large"
+        case photoUrlSmall = "photo_url_small"
+        case sourceUrl = "source_url"
+        case youtubeUrl = "youtube_url"
+    }
+}
+
+struct RecipeResponse: Codable {
+    let recipes: [Recipes]
+}
+
+
